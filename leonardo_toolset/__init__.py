679735a8e1be91fb6be75d52d3c26d82a3167321
Lennart Kowitz <Lennart.Kowitz@isas.de>
2025-06-27 10:40:42 +0200

    version bump, removed old config files

--- leonardo_toolset/__init__.py
+++ leonardo_toolset/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Yu Liu"
 __email__ = "liuyu9671@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0"
 
 
 def get_module_version():
