7c588dfa4ce1b54da9d599fc392cdb31b48157c8
Lennart Kowitz <Lennart.Kowitz@isas.de>
2025-06-30 10:28:27 +0200

    version bump

--- leonardo_toolset/__init__.py
+++ leonardo_toolset/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Yu Liu"
 __email__ = "liuyu9671@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from leonardo_toolset.destripe import DeStripe  # noqa: F401
 from leonardo_toolset.fusion import FUSE_det  # noqa: F401
